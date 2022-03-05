83dcaf3e674a6f54ff0c9d6d2ca2169be6832308
Tee Yu June <56582497+yujune@users.noreply.github.com>
2022-03-05 14:43:50 +0800

    add TodoHeader.

---
+++ MyNote/Modules/Home/Todo/Cell/TodoHeader.swift
@@ -0,0 +1,14 @@
+//
+//  TodoHeader.swift
+//  MyNote
+//
+//  Created by Tee Yu June on 27/02/2022.
+//
+
+import UIKit
+
+class TodoHeader: UITableViewHeaderFooterView {
+    @IBOutlet var headerTitle: UILabel!
+    @IBOutlet var statusLabel: UILabel!
+    
+}
