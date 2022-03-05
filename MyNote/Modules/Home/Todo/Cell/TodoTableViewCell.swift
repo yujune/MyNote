09db56eb86db9c19985569786952d183eda5c7e3
Tee Yu June <56582497+yujune@users.noreply.github.com>
2022-03-05 14:44:01 +0800

    add TodoTableViewCell.

---
+++ MyNote/Modules/Home/Todo/Cell/TodoTableViewCell.swift
@@ -0,0 +1,57 @@
+//
+//  TodoTableViewCell.swift
+//  MyNote
+//
+//  Created by Tee Yu June on 26/02/2022.
+//
+
+import UIKit
+
+protocol TodoTableViewCellDelegate {
+    func checkButtonPress(with todo: Todo)
+}
+
+class TodoTableViewCell: UITableViewCell {
+    
+    var delegate: TodoTableViewCellDelegate?
+    var todoModel: Todo?
+
+    @IBOutlet var checkButton: UIButton! {
+        didSet {
+            checkButton.setTitle("", for: .normal)
+        }
+    }
+    @IBOutlet var todoLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func updateDisplay(with todo: Todo) {
+        todoModel = todo
+        if let isCompleted = todoModel?.isCompleted, isCompleted {
+            checkButton.setImage(UIImage(systemName: "checkmark.circle.fill"), for: .normal)
+        }else {
+            checkButton.setImage(UIImage(systemName: "circle"), for: .normal)
+        }
+        todoLabel.text = todo.name
+    }
+    
+    @IBAction func checkButtonPress(_ sender: UIButton) {
+        guard let todo = todoModel else {
+            return
+        }
+        if let isCompleted = todoModel?.isCompleted, isCompleted {
+            checkButton.setImage(UIImage(systemName: "circle"), for: .normal)
+        }else {
+            checkButton.setImage(UIImage(systemName: "checkmark.circle.fill"), for: .normal)
+        }
+        delegate?.checkButtonPress(with: todo)
+    }
+}
